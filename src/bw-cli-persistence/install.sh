3b2f77b4c8a7bb073d5a19f184d14a63b5d9cda3
Neferites <laurent.alhossri@gmail.com>
2024-02-06 13:52:10 +0100

    feat: add bw cli persistency

---
+++ src/bw-cli-persistence/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "Activating feature 'bitwarden-cli-persistence'"
+echo "User: ${_REMOTE_USER}     User home: ${_REMOTE_USER_HOME}"
+
+if [  -z "$_REMOTE_USER" ] || [ -z "$_REMOTE_USER_HOME" ]; then
+  echo "***********************************************************************************"
+  echo "*** Require _REMOTE_USER and _REMOTE_USER_HOME to be set (by dev container CLI) ***"
+  echo "***********************************************************************************"
+  exit 1
+fi
+
+if [ -e "$_REMOTE_USER_HOME/.config/Bitwarden CLI" ]; then
+  echo "Moving existing .config/Bitwarden CLI folder to .config/Bitwarden CLI-old"
+  mv "$_REMOTE_USER_HOME/.config/Bitwarden CLI" "$_REMOTE_USER_HOME/.config/Bitwarden CLI-old"
+fi
+
+ln -s /dc/bw/ "$_REMOTE_USER_HOME/.config/Bitwarden CLI"
+chown -R "${_REMOTE_USER}:${_REMOTE_USER}" "$_REMOTE_USER_HOME/.config/Bitwarden CLI"
+
+# chown mount (only attached on startup)
+cat << EOF >> "$_REMOTE_USER_HOME/.bashrc"
+sudo chown -R "${_REMOTE_USER}:${_REMOTE_USER}" /dc/bw
+EOF
+chown -R $_REMOTE_USER $_REMOTE_USER_HOME/.bashrc
--- test/_global/all.sh
+++ test/_global/all.sh
@@ -19,6 +19,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
 check "novops version" novops --version
+check "bw version" bw --version
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
