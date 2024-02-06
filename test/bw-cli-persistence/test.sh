ac084e3016608de131e82d62400c8d270d131560
Neferites <laurent.alhossri@gmail.com>
2024-02-06 14:15:19 +0100

    fix: test bw persitence

--- test/bw-cli-persistence/test.sh
+++ test/bw-cli-persistence/test.sh
@@ -12,6 +12,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
+check "bw persistence" ls -lah ~/.
 check "bw version" bw --version
 
 # Report result
